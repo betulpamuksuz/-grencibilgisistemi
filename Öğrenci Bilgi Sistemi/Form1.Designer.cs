5c220697f5ac4be5ba5c8e675326f91fced8fcb2
betulpamuksuz <165507744+betulpamuksuz@users.noreply.github.com>
2024-12-12 21:38:07 +0100

    Add files via upload

---
+++ Öğrenci Bilgi Sistemi/Form1.Designer.cs
@@ -0,0 +1,445 @@
+﻿namespace BilgiSistem
+{
+    partial class btnGoster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnOnay;
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ad = new System.Windows.Forms.Label();
+            this.Soyad = new System.Windows.Forms.Label();
+            this.No = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DersAdı = new System.Windows.Forms.Label();
+            this.txtDersAdı = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numKredi = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numKontejan = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstSecilen = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtKrediDeger = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lstOgrenci = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboRapor = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            btnOnay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numKredi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numKontejan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğrenci Kayıt Formu";            // 
+            // Ad
+            // 
+            this.Ad.AutoSize = true;
+            this.Ad.Location = new System.Drawing.Point(30, 61);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(27, 16);
+            this.Ad.TabIndex = 1;
+            this.Ad.Text = "Ad:";
+            // 
+            // Soyad
+            // 
+            this.Soyad.AutoSize = true;
+            this.Soyad.Location = new System.Drawing.Point(30, 95);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(50, 16);
+            this.Soyad.TabIndex = 2;
+            this.Soyad.Text = "Soyad:";
+            // 
+            // No
+            // 
+            this.No.AutoSize = true;
+            this.No.Location = new System.Drawing.Point(30, 131);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(28, 16);
+            this.No.TabIndex = 3;
+            this.No.Text = "No:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(86, 61);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(76, 22);
+            this.txtAd.TabIndex = 4;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(86, 92);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(75, 22);
+            this.txtSoyad.TabIndex = 5;
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(86, 128);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(76, 22);
+            this.txtNo.TabIndex = 6;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(36, 169);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(124, 35);
+            this.btnKaydet.TabIndex = 7;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ders Ekleme Düzenleme";
+            // 
+            // DersAdı
+            // 
+            this.DersAdı.AutoSize = true;
+            this.DersAdı.Location = new System.Drawing.Point(228, 59);
+            this.DersAdı.Name = "DersAdı";
+            this.DersAdı.Size = new System.Drawing.Size(62, 16);
+            this.DersAdı.TabIndex = 9;
+            this.DersAdı.Text = "Ders Adı:";
+            // 
+            // txtDersAdı
+            // 
+            this.txtDersAdı.Location = new System.Drawing.Point(305, 55);
+            this.txtDersAdı.Name = "txtDersAdı";
+            this.txtDersAdı.Size = new System.Drawing.Size(88, 22);
+            this.txtDersAdı.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(229, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Kredi:";
+            // 
+            // numKredi
+            // 
+            this.numKredi.Location = new System.Drawing.Point(309, 92);
+            this.numKredi.Name = "numKredi";
+            this.numKredi.Size = new System.Drawing.Size(84, 22);
+            this.numKredi.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(230, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Kontejan:";
+            // 
+            // numKontejan
+            // 
+            this.numKontejan.Location = new System.Drawing.Point(314, 126);
+            this.numKontejan.Name = "numKontejan";
+            this.numKontejan.Size = new System.Drawing.Size(78, 22);
+            this.numKontejan.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 38);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Ders Ekle Düzenle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(476, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Ders Seçim Formu";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(485, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Seçilen Dersler:";
+            // 
+            // lstSecilen
+            // 
+            this.lstSecilen.FormattingEnabled = true;
+            this.lstSecilen.ItemHeight = 16;
+            this.lstSecilen.Location = new System.Drawing.Point(605, 62);
+            this.lstSecilen.Name = "lstSecilen";
+            this.lstSecilen.Size = new System.Drawing.Size(132, 36);
+            this.lstSecilen.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(487, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Kredi:";
+            // 
+            // txtKrediDeger
+            // 
+            this.txtKrediDeger.Location = new System.Drawing.Point(554, 134);
+            this.txtKrediDeger.Name = "txtKrediDeger";
+            this.txtKrediDeger.Size = new System.Drawing.Size(130, 22);
+            this.txtKrediDeger.TabIndex = 20;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(489, 185);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(116, 34);
+            this.btnEkle.TabIndex = 21;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(645, 190);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(119, 28);
+            this.btnSil.TabIndex = 22;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 284);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(144, 16);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Danışman Onay Formu";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 320);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 16);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Öğrenci Listesi:";
+            // 
+            // lstOgrenci
+            // 
+            this.lstOgrenci.FormattingEnabled = true;
+            this.lstOgrenci.ItemHeight = 16;
+            this.lstOgrenci.Location = new System.Drawing.Point(147, 316);
+            this.lstOgrenci.Name = "lstOgrenci";
+            this.lstOgrenci.Size = new System.Drawing.Size(123, 20);
+            this.lstOgrenci.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(29, 358);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(102, 16);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Seçilen Dersler:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(147, 358);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(123, 20);
+            this.listBox1.TabIndex = 27;
+            // 
+            // btnOnay
+            // 
+            btnOnay.Location = new System.Drawing.Point(42, 408);
+            btnOnay.Name = "btnOnay";
+            btnOnay.Size = new System.Drawing.Size(105, 36);
+            btnOnay.TabIndex = 28;
+            btnOnay.Text = "Onayla";
+            btnOnay.UseVisualStyleBackColor = true;
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(184, 407);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(121, 36);
+            this.btnGeri.TabIndex = 29;
+            this.btnGeri.Text = "Geri Gönder";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(489, 264);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(163, 16);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "Ders ve Öğrenci Raporları";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(472, 305);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(86, 16);
+            this.label12.TabIndex = 31;
+            this.label12.Text = "Rapor Seçim";
+            // 
+            // comboRapor
+            // 
+            this.comboRapor.FormattingEnabled = true;
+            this.comboRapor.Location = new System.Drawing.Point(593, 309);
+            this.comboRapor.Name = "comboRapor";
+            this.comboRapor.Size = new System.Drawing.Size(238, 24);
+            this.comboRapor.TabIndex = 32;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(497, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 30);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Göster";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnGoster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 544);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboRapor);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnGeri);
+            this.Controls.Add(btnOnay);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lstOgrenci);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtKrediDeger);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lstSecilen);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numKontejan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numKredi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDersAdı);
+            this.Controls.Add(this.DersAdı);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.Soyad);
+            this.Controls.Add(this.Ad);
+            this.Controls.Add(this.label1);
+            this.Name = "btnGoster";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numKredi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numKontejan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Ad;
+        private System.Windows.Forms.Label Soyad;
+        private System.Windows.Forms.Label No;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label DersAdı;
+        private System.Windows.Forms.TextBox txtDersAdı;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numKredi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numKontejan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox lstSecilen;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtKrediDeger;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox lstOgrenci;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnGeri;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox comboRapor;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Öğrenci Bilgi Sistemi/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BilgiSistem
+{
+    public partial class btnGoster : Form
+    {
+        public btnGoster()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            // Kullanıcıdan alınan veriler
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            string numara = txtNo.Text;
+
+            // SQL bağlantı dizesi
+            string connectionString = "Server=localhost;Database=BilgiSistemDB;Integrated Security=True;";
+
+            // Veritabanına veri ekleme SQL sorgusu
+            string query = "INSERT INTO Ogrenciler (Ad, Soyad, Numara) VALUES (@Ad, @Soyad, @Numara)";
+
+            try
+            {
+                // Bağlantıyı oluştur
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // SQL komutunu oluştur
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Parametreler ile SQL sorgusunu güvenli hale getir
+                    command.Parameters.AddWithValue("@Ad", ad);
+                    command.Parameters.AddWithValue("@Soyad", soyad);
+                    command.Parameters.AddWithValue("@Numara", numara);
+
+                    // Bağlantıyı aç
+                    connection.Open();
+
+                    // Komutu çalıştır (veriyi ekle)
+                    command.ExecuteNonQuery();
+
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
